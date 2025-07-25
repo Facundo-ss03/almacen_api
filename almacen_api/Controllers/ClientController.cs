b2b8aabd20f6da264edf5c6894d0b2862d8b1ac2
Facundo Sosa <js1697436@gmail.com>
2025-07-25 20:45:38 -0300

    creación del proyecto ASP.NET + carpeta controller

---
+++ almacen_api/Controllers/ClientController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using almacen_api.Models;
+
+namespace almacen_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetClient")]
+        public Client GetClient()
+        {
+
+            Client client = new Client
+            {
+                Id = 1,
+                Name = "Lionel",
+                LastName = "Messi"
+
+            };
+
+            return client;
+        }
+    }
+}
